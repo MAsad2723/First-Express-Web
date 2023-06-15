3f122e3ffd50ec2bdc835dee3c9a714fb56e0ad2
k213458 <k213458@example.com>
2023-06-15 22:09:25 +0500

    Added All Files

--- Express/app.js
+++ Express/app.js
@@ -20,8 +20,7 @@ app.post('/', (req, res) => {
     const gender = req.body.gender;
     const address = req.body.address;
     const more = req.body.more;
-    res.send(`<h1>Details of User:<h1><h2>Name : ${Name}.</h2><br><h2>Age : ${age}.</h2><br><h2>Gender : ${gender}.</h2><br><h2>Address : ${address}.</h2><br><h2>More details : ${more}.</h2><button onclick="window.location.href = '/Express/static'">Go to Orginal Page</button>`);
-    // res.sendFile(__dirname + '/static/index.html');
+    res.sendFile(__dirname + '/static/index.html');
 })
 // Start the server
 const port = 3000;
