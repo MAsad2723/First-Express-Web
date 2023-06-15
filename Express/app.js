39159e37ee4977a3f3a4d7c052a3f8c6453cfc26
k213458 <k213458@example.com>
2023-06-15 21:17:39 +0500

    Added All Files

--- Express/app.js
+++ Express/app.js
@@ -1,35 +1,31 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const fs = require('fs');
-const port = 3000;
-//EXPRESS RELATED STUFF
-app.use('/static', express.static('static'));
-app.use(express.urlencoded());
-//PUG RELATED STUFF
-app.set('view engine', 'pug');//Important
-app.set('views', path.join(__dirname, 'views'));//Set the views directory
+const bodyParser = require('body-parser');
 
-//ENDPOINTS
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the "static" directory
+app.use(express.static('static'));
+// app.use(express.urlencoded());
+
+// Define a route for the homepage
 app.get('/', (req, res) => {
-    const con = "Get this membership for 59.99$ only. Fill this form now!";
-    const params = { 'title': 'PubG is the best game', 'content': con }
-    res.status(200).render('index.pug', params);
-})
-app.post('/', (req, res)=>{
-    Name = req.body.name;
-    age = req.body.age;
-    gender = req.body.gender;
-    address = req.body.address;
-    more = req.body.more;
+    res.sendFile(__dirname + '/static/index.html');
+});
+
+app.post('/', (req, res) => {
+    const Name = req.body.name;
+    const age = req.body.age;
+    const gender = req.body.gender;
+    const address = req.body.address;
+    const more = req.body.more;
     let str = `Details of User:\n\tName : ${Name}.\n\tAge : ${age}.\n\tGender : ${gender}.\n\tAddress : ${address}.\n\tMore details : ${more}.`;
     fs.writeFileSync('output.txt', str);
-    const con = "Get this membership for 59.99$ only. Fill this form now!";
-    const params = { 'title': 'PubG is the best game', 'content': con ,'message': 'Form submitted Successfully'}
-    res.status(200).render('index.pug', params);
+    res.sendFile(__dirname + '/static/index.html');
 })
-
-//START THE SERVER
+// Start the server
+const port = 3000;
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
-})
+    console.log(`Server running on port ${port}`);
+});
